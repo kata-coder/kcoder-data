adfd59a377f23ea5a7c6cec8add7179f6b24816f
Alex Dukhno <Alex-Diez@users.noreply.github.com>
2018-04-14 21:43:40 +0300

    Set up ci (#1)
    * added travis config
    
    * configured gradle with junit 5
    
    * replaced junit 4 tests with junit 5
    
    * setup cobertura travis coveralls integration

--- src/test/java/io/github/dmitry404/kcoderdata/ExercisesDataTests.java
+++ src/test/java/io/github/dmitry404/kcoderdata/ExercisesDataTests.java
@@ -1,18 +1,18 @@
 package io.github.dmitry404.kcoderdata;
 
 import io.github.dmitry404.kcoderdata.domain.Exercise;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ExercisesDataTests {
   @Autowired
--- src/test/java/io/github/dmitry404/kcoderdata/ExercisesServiceTests.java
+++ src/test/java/io/github/dmitry404/kcoderdata/ExercisesServiceTests.java
@@ -1,22 +1,18 @@
 package io.github.dmitry404.kcoderdata;
 
-import io.github.dmitry404.kcoderdata.domain.Exercise;
-import io.github.dmitry404.kcoderdata.repositories.ExercisesRepository;
-import io.github.dmitry404.kcoderdata.services.ExercisesDataService;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
+
+import io.github.dmitry404.kcoderdata.domain.Exercise;
+import io.github.dmitry404.kcoderdata.repositories.ExercisesRepository;
+import io.github.dmitry404.kcoderdata.services.ExercisesDataService;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
